0dcfa0a4fdcb368f7d826e872cb0b647dbcff2e9
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/PrintDXGridSample/MainWindowViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+// ...
+
+namespace PrintDXGridSample {
+    public class MainWindowViewModel {
+        IEnumerable<Person> persons;
+
+        public IEnumerable<Person> Persons {
+            get {
+                return persons;
+            }
+            set {
+                persons = value;
+            }
+        }
+
+        public MainWindowViewModel() {
+            persons = new List<Person>(new Person[] { 
+                new Person("John", 25),
+                new Person("Mary", 33)
+            });
+        }
+    }
+}
---
+++ CS/PrintDXGridSample/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+// ...
+
+namespace PrintDXGridSample {
+    public class Person {
+        public string Name { get; private set; }
+        public int Age { get; private set; }
+
+        public Person(string name, int age) {
+            this.Name = name;
+            this.Age = age;
+        }
+    }
+}
