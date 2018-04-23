900bd68c0d26ee0a8302a015267d836fcf2e6be2
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/PrintDXGridSample/MainWindowViewModel.vb
@@ -0,0 +1,15 @@
+﻿Imports System.Collections.Generic
+' ...
+
+Namespace PrintDXGridSample
+    Public Class MainWindowViewModel
+        Public Property Persons() As IEnumerable(Of Person)
+
+        Public Sub New()
+            Persons = { _
+                New Person("John", 25), _
+                New Person("Mary", 33) _
+            }
+        End Sub
+    End Class
+End Namespace
